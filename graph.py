e2a6e73fcd6ae3b6c4dc70300ba887a9c3edcb9b
arauzo <arauzo@3ee892a3-3a47-0410-8b82-87159df38f27>
2008-03-21 11:00:12 +0000

    Copyright and title of each file.

--- graph.py
+++ graph.py
@@ -1,23 +1,23 @@
-#! /usr/bin/env python
-# -*- coding: iso-8859-15 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#PPC-PROJECT. HERRAMIENTA MULTIPLATAFORMA PARA LA 
-#DOCENCIA E INVESTIGACIÓN EN GESTIÓN DE PROYECTOS
-#Copyright (C) 2007 UNIVESIDAD DE CÓRDOBA
-#Author: CRISTINA URBANO ROLDÁN
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-
- #This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Graph functions
+#-----------------------------------------------------------------------
+# PPC-PROJECT
+#   Multiplatform software tool for education and research in 
+#   project management
+#
+# Copyright 2007-8 Universidad de Córdoba
+# This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published
+#   by the Free Software Foundation, either version 3 of the License,
+#   or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import math, os, sys
 from pert import *
