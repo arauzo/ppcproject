236fcdb352927bf3e017121e50f94338cec36741
negralexx <negralexx@3ee892a3-3a47-0410-8b82-87159df38f27>
2008-04-22 21:46:59 +0000

    GUI of COMSOAL window changed. Comment correctly the functions. More minor changes.

--- simAnnealing.py
+++ simAnnealing.py
@@ -3,21 +3,21 @@ import copy
 from math import exp
 from graph import successors2precedents
 
-#-----------------------------------------------------------
-# Create a dictionary with the resources availability.
-# Works only with Renewable and Doubly restricted resources
-#
-# Parameters: -
-#
-# Return value: resources 
-#-----------------------------------------------------------
 
-def resourcesAvailability(recursos):
+def resourcesAvailability(availableResources):
+    """
+    Create a dictionary with the renewable and the
+            doubly restricted resources
+
+    Parameters: availableResources (list of resources in the project)
+
+    Returned value: resources
+    """
 
     resources={} 
    
     #Create a dictionary with the resources' name and number available   
-    for a in recursos:
+    for a in availableResources:
         if a[1] == 'Renovable':
             resources[a[0]] = a[3]
         elif a[1] == 'Doblemente restringido':
@@ -25,21 +25,21 @@ def resourcesAvailability(recursos):
    
     return resources
 
-#-----------------------------------------------------------
-# Create a dictionary with the resources
-#          the activities need
-#
-# Parameters: -
-#
-# Return value: asignation 
-#-----------------------------------------------------------
 
-def resourcesPerActivities(asignacion):
+def resourcesPerActivities(initialAsignation):
+    """
+    Create a dictionary with the resources
+          the activities need
+
+    Parameters: initialAsignation (list of the activities and the resources they need) 
+
+    Returned value: asignation
+    """
       
     asignation={}
-      
+    
     #Create a dictionary with the activities and the resources they need
-    for a in asignacion:
+    for a in initialAsignation:
         if a[0] in asignation.keys():
             asignation[a[0]] = asignation[a[0]] + [(a[1],a[2])]
         else:
@@ -47,77 +47,72 @@ def resourcesPerActivities(asignacion):
      
     return asignation
       
-#-----------------------------------------------------------
-# Generate or modify a planning. It depends on currentTime. 
-#
-#
-# Parametros: asignation (dictionary of activities and the resources they need)
-#             resources (dictionary of the resources and their availability)
-#
-# Valor de retorno: result 
-#-----------------------------------------------------------
-def simulatedAnnealing(asignation,resources,successors,activities,balance,temperature,minTemperature,k,numIteraciones):
+def simulatedAnnealing(asignation,resources,successors,activities,balance,temperature,minTemperature,k,numIterations):
+    """
+    Try to find the best planning of the project by
+        the technique of simulated annealing
 
+    Parameters: asignation (returned by resourcesPerActivity)
+                resources (returned by resourcesAvailability)
+                successors (the prelations of the activities)
+                activities (dictionary with the name of activities and their characteristics)
+                balance (if 0 it will allocate else it will balance)
+                temperature (parameter to configure simulated annealing algorithm)
+                minTemperature ((parameter to configure simulated annealing algorithm)
+                k ((parameter to configure simulated annealing algorithm)
+                numIterations ((parameter to configure simulated annealing algorithm)
+
+    Returned value: (prog1,prog1Evaluated) or (progAux,progAuxEvaluated) (the best planning and the duration/variance of the project)
+    """
+    
     predecessors = successors2precedents(successors)
 
     for a in predecessors.copy():
         if predecessors[a] == []:
             del predecessors[a]
 
-    # Con estos datos realiza 153,9 reducciones de temperatura
-    #temperature = 100000
-    #k = 0.9
-    #minTemperature = 0.01
-    #numIteraciones = 100
-   
-
     # prog1 will store the best planning
-    prog1=generate(asignation,resources.copy(),copy.deepcopy(predecessors),activities.copy(),balance)
-    prog1Evaluated = evaluate(prog1,balance,asignation,resources)
+    progAux = prog1 = generate(asignation,resources.copy(),copy.deepcopy(predecessors),activities.copy(),balance)
+    progAuxEvaluated = prog1Evaluated = evaluate(prog1,balance,asignation,resources)
       
-    while temperature > minTemperature and numIteraciones > 0: # XXX numIteraciones sin mejora infinito
+    while temperature > minTemperature and numIterations != 0: # XXX grafica?
       
-        prog2=modify(asignation,resources.copy(),copy.deepcopy(predecessors),activities.copy(),prog1,balance)
-      
+        prog2 = modify(asignation,resources.copy(),copy.deepcopy(predecessors),activities.copy(),prog1,balance)
         prog2Evaluated = evaluate(prog2,balance,asignation,resources)
-
-        print 'resultado1: ',prog1Evaluated,' resultado2: ',prog2Evaluated
          
         if prog2Evaluated <= prog1Evaluated:
             prog1 = prog2
             prog1Evaluated = prog2Evaluated
         else:
-            numIteraciones -= 1
+            numIterations -= 1
             r = random.random()
-            #print 'r: ',r
             m = exp(-(prog2Evaluated*10000-prog1Evaluated*10000) / temperature)
-            #print 'm: ',m
             if r < m:
-                print 'Escojo sol peor'
+                progAux = prog1 # XXX Lorenzo dijo de quedarnos con la mejor solucion de todas aunque se fuese por un camino peor
+                progAuxEvaluated = prog1Evaluated
                 prog1 = prog2
-                prog1Evaluated = prog2Evaluated 
-      
+                prog1Evaluated = prog2Evaluated       
         temperature = k * temperature
-        #print 'temperatura: ',temperature
-   
-    if numIteraciones == 0:
-        print 'Se corto por numIteraciones'
+
+    if prog1Evaluated <= progAuxEvaluated:
+        return (prog1,prog1Evaluated)
     else:
-        print 'Se corto por temperatura', temperature
-      
-    print prog1
-    print prog1Evaluated
-#-----------------------------------------------------------
-# Generate a planning. 
-#
-#
-# Parametros: asignation (dictionary of activities and the resources they need)
-#             resources (dictionary of the resources and their availability)
-#
-# Valor de retorno: result 
-#-----------------------------------------------------------  
+        return (progAux,progAuxEvaluated)
+
+
 def generate(asignation,resources,predecessors,activities,balance):
-   
+    """
+    Generate a planning
+
+    Parameters: asignation (returned by resourcesPerActivity)
+                resources (returned by resourcesAvailability)
+                predecessors (dictionary with activities and their predecessors)
+                activities (dictionary with the name of activities and their characteristics)
+                balance (if 0 it will allocate else it will balance)
+
+    Returned value: prog (planing generated)
+    """
+       
     currentTime = 0
     possibles = {}
     executing = {}
@@ -126,17 +121,21 @@ def generate(asignation,resources,predecessors,activities,balance):
     prog=generateOrModify(asignation,resources,predecessors,activities,balance,possibles,executing,result,currentTime)
     return prog
 
-#-----------------------------------------------------------
-# Modify a planning. 
-#
-#
-# Parametros: asignation (dictionary of activities and the resources they need)
-#             resources (dictionary of the resources and their availability)
-#
-# Valor de retorno: result 
-#-----------------------------------------------------------  
+
 def modify(asignation,resources,predecessors,activities,prog1,balance):
-   
+    """
+    Modify a planning
+
+    Parameters: asignation (returned by resourcesPerActivity)
+                resources (returned by resourcesAvailability)
+                predecessors (dictionary with activities and their predecessors)
+                activities (dictionary with the name of activities and their characteristics)
+                prog1 (planning to modify)
+                balance (if 0 it will allocate else it will balance)
+
+    Returned value: prog (planning modified)
+    """
+       
     result = []
     executing = {}
     possibles = {}
@@ -144,8 +143,6 @@ def modify(asignation,resources,predecessors,activities,prog1,balance):
    
     # The modification will start at this time
     currentTime = random.randint(0, prog1[-2][1])
-    #print 'activities: ',activities
-    #print 'tiempo actual: ',currentTime
    
     # Update the state of result, executing, resources and activities to the state they would be at currentTime
     for act,startTime,endTime in prog1:
@@ -156,11 +153,8 @@ def modify(asignation,resources,predecessors,activities,prog1,balance):
                 if act in asignation.keys() and balance == 0:
                     for resource,amount in asignation[act]:
                         resources[resource] = float(resources[resource]) - float(amount)
-            activities.pop(act) # falta pasar de activities a possibles
-   
-    #print 'resultado mod: ', result
-    #print 'ejecutando mod: ', executing
-    #print 'actividad mod: ', activities
+            activities.pop(act) 
+
     # Update the state of predecessors to the state it would be at currentTime
     for act,startTime,endTime in result:
         if act not in executing:
@@ -174,41 +168,52 @@ def modify(asignation,resources,predecessors,activities,prog1,balance):
     for a in activities.copy():
         if a not in predecessors.keys(): 
             possibles[a] = activities.pop(a)   
-    #print 'predecessors mod: ', predecessors
-    #print 'possibles mod: ', possibles
       
-    prog=generateOrModify(asignation,resources,predecessors,activities,balance,possibles,executing,result,currentTime)
+    prog = generateOrModify(asignation,resources,predecessors,activities,balance,possibles,executing,result,currentTime)
     return prog
 
-#-----------------------------------------------------------
-# Evaluate a planning. 
-#
-#
-# Parametros: asignation (dictionary of activities and the resources they need)
-#             resources (dictionary of the resources and their availability)
-#
-# Valor de retorno: result 
-#-----------------------------------------------------------
-def evaluate(prog,balance,asignation,resources):
 
+def evaluate(prog,balance,asignation,resources):
+    """
+    Evaluate a planning
+
+    Parameters: prog (planning to evaluate)
+                balance (if 0 it will consider resoucers)
+                asignation (returned by resourcesPerActivity)
+                resources (returned by resourcesAvailability)
+                
+
+    Returned value: duration or variance (duration or variance of the planning evaluated)
+    """
+    
     duration = 0
   
     # Calculate duration
     for act,startTime,endTime in prog:
         if endTime > duration:
             duration = endTime
-
-    print 'duracion: ', duration 
+ 
     if balance == 0: # if allocate
-        return duration
-         
+        return duration   
     else: #if balance
         variance = calculateVariance(prog,resources,asignation,duration) 
         return variance
       
       
 def calculateVariance(prog,resources,asignation,duration): 
+    """
+    Calculate the variance of the planning it receives
 
+    Parameters: prog (planning to evaluate)
+                resources (returned by resourcesAvailability)
+                asignation (returned by resourcesPerActivity)
+                duration (duration of the planning)
+                
+                
+
+    Returned value: variance (the average of all resources' variance)
+    """
+    
     # Calculate the loading sheet for each resource for prog1
     loadingSheet = {}
     average = {}
@@ -228,10 +233,8 @@ def calculateVariance(prog,resources,asignation,duration):
             if resource in loadingSheet.keys():
                 loadingSheet[resource] += [(time,amount)]
             else:
-                loadingSheet[resource] = [(time,amount)]
-            
+                loadingSheet[resource] = [(time,amount)]  
             average[resource] += amount
-        
         average[resource] = average[resource] / duration
       
     for resource in resources:
@@ -242,44 +245,37 @@ def calculateVariance(prog,resources,asignation,duration):
         finalVariance += variance[resource]
       
     return finalVariance / len(resources)
-      
-                    
-            
-   
-#-----------------------------------------------------------
-# Generate or modify a planning. It depends on currentTime. 
-#
-#
-# Parametros: asignation (dictionary of activities and the resources they need)
-#             resources (dictionary of the resources and their availability)
-#             predecessors (list of the relation of the activities)
-#             activities (list of activities, their duration and their maximum time to start)
-#             balance (flag if it will balance or allocate resources)
-#             possibles (dictionary of possibles activities to execute)
-#             executing (dictionary of activities executing)
-#             result (list with the planning)
-#             currentTime (time in which is the algorithm)
-# Valor de retorno: result 
-#-----------------------------------------------------------   
+         
+
 def generateOrModify(asignation,resources,predecessors,activities,balance,possibles,executing,result,currentTime):
-   
-   
+    """
+    Generate or modify a planning.
+    It depends on currentTime
+
+    Parameters: asignation (returned by resourcesPerActivity)
+                resources (returned by resourcesAvailability)
+                predecessors (dictionary with activities and their predecessors)
+                activities (dictionary with the name of activities and their characteristics)
+                balance (if 0 it will allocate else it will balance)
+                possibles (dictionary of the activities which can execute at currentTime)
+                executing (dictionary of the activities which are executing at currentTime)
+                result (planning of the project up to currentTime)
+                currentTime (time when the algorithm starts to generate a new planning) 
+
+    Returned value: result (planning calculated)
+    """
+    
     lengthResources = len(resources)
-    if lengthResources == 0: #XXX prueba limite recursos lista vacia->prog minima
+    if lengthResources == 0: 
         lengthResources = -1 
    
     resourcesUsedUp = 0 
    
-   
     while activities != {} or possibles != {} or executing != {}: # Until all the activities have finished
-        #print 'currentTime: ',currentTime
         # Choose activities with no predecessors
         for a in activities.copy():
             if a not in predecessors.keys():
                 possibles[a] = activities.pop(a)
-      
-        #print 'possibles: ', possibles
-        #print 'predecessores :', predecessors
       
         if possibles != {}:
 
@@ -290,7 +286,6 @@ def generateOrModify(asignation,resources,predecessors,activities,balance,possib
                         executing[act] = possibles[act][0]
                         result += [(act, currentTime, currentTime + possibles[act][0])]
                         del possibles[act]
-
                 # Calculate a number of activities to execute       
                 # A minimum of one activity has to be executing
                 if executing == {}:
@@ -299,7 +294,7 @@ def generateOrModify(asignation,resources,predecessors,activities,balance,possib
                     numActivities = random.randint(0,len(possibles))
             else:
                 numActivities = -1
-         
+       
             possiblesCopy = possibles.copy()
          
             # Execute activities until a number of activities have executed, no more possibles activities or no more resources 
@@ -307,7 +302,6 @@ def generateOrModify(asignation,resources,predecessors,activities,balance,possib
             
                 noExecute = 0
 
-                #print 'possiblesCopy ',possiblesCopy
                 # Pop a random item of possiblesCopy
                 possiblesKeys = possiblesCopy.keys()
                 key = random.choice(possiblesKeys)
@@ -315,14 +309,11 @@ def generateOrModify(asignation,resources,predecessors,activities,balance,possib
  
                 # If resources are required            
                 if key in asignation.keys() and balance == 0:
-                    #print 'recursos: ', resources
                     for resource,amount in asignation[key]:
-                        #print 'recurso disponible: ',resources[resource],'cantidad: ',amount
                         # if resource required > resource available
                         if float(amount) > float(resources[resource]):
                             noExecute = 1
-                            break
-                     
+                            break                     
                     if noExecute == 0:
                         for resource,amount in asignation[key]:
                             resources[resource] = float(resources[resource]) - float(amount)
@@ -330,16 +321,12 @@ def generateOrModify(asignation,resources,predecessors,activities,balance,possib
                                 resourcesUsedUp += 1          
                         executing[key] = act[0] # Add activity in executing
                         result += [(key, currentTime, currentTime + act[0])] # Update result
-                        del possibles[key] # Remove the activity from possibles dictionary 
-            
+                        del possibles[key] # Remove the activity from possibles dictionary             
                 else: # The activity doesn't consumed any resource
                     executing[key] = act[0]
                     result += [(key, currentTime, currentTime + act[0])]
                     del possibles[key] #remove the activity from possibles dictionary
-                    numActivities -= 1
-            
-                #print 'executing ', executing
-                #print 'result ', result      
+                    numActivities -= 1      
       
         currentTime += 1
 
@@ -361,7 +348,7 @@ def generateOrModify(asignation,resources,predecessors,activities,balance,possib
                         predecessors[act].remove(a)
                         if predecessors[act]==[]:
                             del predecessors[act]
-    #print result               
+              
     return result
     
 
