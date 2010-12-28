56dba2e14815725571ea0a093a21968713c3856e
i22javier <i22javier@3ee892a3-3a47-0410-8b82-87159df38f27>
2010-12-28 00:31:40 +0000

    modificacion pruebaAC para que tenga parametros de entrada

--- pruebaAC.py
+++ pruebaAC.py
@@ -6,7 +6,8 @@ import algoritmoCohenSadeh
 import graph1
 import fileFormats
 
-REPETICIONES = 1000
+#REPETICIONES = 1
+#REPETICIONES = int(sys.argv[2])
 def openProject(filename):
     """
     Open a project file given by filename
@@ -46,131 +47,140 @@ def openProject(filename):
         print 'Error reading file:', filename
         sys.exit(1)
 
+if len(sys.argv)==3:
+    repeticiones= int(sys.argv[2])
+    filename=sys.argv[1]
 
+    data = openProject(filename)
+    successors = {}
 
-filename = "j1201_1.sm"
-data = openProject(filename)
-successors = {}
+    for i in data:
+        successors[i[1]]=i[2]
 
-for i in data:
-    successors[i[1]]=i[2]
+    prelaciones1 = graph1.reversedGraph(successors)
+    prelaciones = {
+        'B': [], 
+        'A': [], 
+        'D': ['B'], 
+        'C': [], 
+        'F': ['C'], 
+        'E': ['D'], 
+        'H': ['B'], 
+        'G': ['F'], 
+        'J': ['F'], 
+        'I': ['A'], 
+        'L': ['C', 'E'], 
+        'K': ['I'], 
+        'N': ['B'], 
+        'M': ['H'], 
+        'P': ['D'], 
+        'O': ['E'], 
+        'R': ['F'], 
+        'Q': ['E'], 
+        'S': ['O'], 
+        'T': ['J', 'N', 'P'], 
+        'U': ['I'], 
+        'V': ['Q'], 
+        'W': ['K'],
+        'X': ['L', 'M', 'R'], 
+        'Y': ['J', 'P', 'O'], 
+        'Z': ['Y', 'U', 'G'], 
+        'AB': ['H'], 
+        'AC': ['W', 'U', 'P'],
+        'AD': ['X', 'Z', 'AB'],
+        'AE': ['S', 'T', 'V']
+        }
+    prelaciones = {
+        'A' : [],
+        'B' : [],
+        'C' : ['A','B'],
+        'D' : ['A'],
+        'E' : ['B'],
+        'F' : ['A','B'],
+        'G' : ['C'],
+        'H' : ['D','E'],
+        'I' : ['D','E','F'],
+        'J' : ['D','E','F'],
+        'K' : ['D','F','L'],
+        'L' : ['A'],
+        }
+    prelaciones = {
+        'A' : [],
+        'B' : [],
+        'C' : ['A'],
+        'D' : ['A','B'],
+        'E' : ['C','D'],
+        'F' : ['D'],
+        'G' : ['E'],
+        'H' : ['F','D','C'],
+        'I' : ['F'],
+        }
+    prelaciones = {
+        'A' : [],
+        'B' : [],
+        'C' : [],
+        'D' : ['A'],
+        'E' : ['B'],
+        'F' : ['C'],
+        'G' : ['E','F'],
+        'H' : ['D'],
+        }
 
-prelaciones1 = graph1.reversedGraph(successors)
-prelaciones = {
-    'B': [], 
-    'A': [], 
-    'D': ['B'], 
-    'C': [], 
-    'F': ['C'], 
-    'E': ['D'], 
-    'H': ['B'], 
-    'G': ['F'], 
-    'J': ['F'], 
-    'I': ['A'], 
-    'L': ['C', 'E'], 
-    'K': ['I'], 
-    'N': ['B'], 
-    'M': ['H'], 
-    'P': ['D'], 
-    'O': ['E'], 
-    'R': ['F'], 
-    'Q': ['E'], 
-    'S': ['O'], 
-    'T': ['J', 'N', 'P'], 
-    'U': ['I'], 
-    'V': ['Q'], 
-    'W': ['K'],
-    'X': ['L', 'M', 'R'], 
-    'Y': ['J', 'P', 'O'], 
-    'Z': ['Y', 'U', 'G'], 
-    'AB': ['H'], 
-    'AC': ['W', 'U', 'P'],
-    'AD': ['X', 'Z', 'AB'],
-    'AE': ['S', 'T', 'V']
-    }
-prelaciones = {
-    'A' : [],
-    'B' : [],
-    'C' : ['A','B'],
-    'D' : ['A'],
-    'E' : ['B'],
-    'F' : ['A','B'],
-    'G' : ['C'],
-    'H' : ['D','E'],
-    'I' : ['D','E','F'],
-    'J' : ['D','E','F'],
-    'K' : ['D','F','L'],
-    'L' : ['A'],
-    }
-prelaciones = {
-    'A' : [],
-    'B' : [],
-    'C' : ['A'],
-    'D' : ['A','B'],
-    'E' : ['C','D'],
-    'F' : ['D'],
-    'G' : ['E'],
-    'H' : ['F','D','C'],
-    'I' : ['F'],
-    }
-prelaciones = {
-    'A' : [],
-    'B' : [],
-    'C' : [],
-    'D' : ['A'],
-    'E' : ['B'],
-    'F' : ['C'],
-    'G' : ['E','F'],
-    'H' : ['D'],
-    }
+    itime=os.times()
+    for i in range(repeticiones):
+        g = algoritmoCohenSadeh.cohenSadeh(prelaciones1)
+    ftime=os.times()
+    utime = ftime[0] - itime[0]
+    print "CohenSadeh"
+    print "utime %.4f"% (utime)
+    print "numero de nodos: ",g.numNodes()
+    print "numero de arcos: ",g.numArcs()
+    print "numero de arcos reales: ",g.numArcsReales()
+    print "numero de arcos ficticios: ",g.numArcsFicticios()
+    print
+    image_text = graph1.pert2image(g, format='png')
+    fsalida = open('CohenSadeh' + filename + '.png', 'w')
+    fsalida.write(image_text)
+    fsalida.close()
 
-itime=os.times()
-for i in range(REPETICIONES):
-    g = algoritmoCohenSadeh.cohenSadeh(prelaciones1)
-ftime=os.times()
-utime = ftime[0] - itime[0]
-print "CohenSadeh"
-print "utime %.4f"% (utime)
-print "numero de nodos: ",g.numNodes()
-print "numero de arcos: ",g.numArcs()
-print "numero de arcos reales: ",g.numArcsReales()
-print "numero de arcos ficticios: ",g.numArcsFicticios()
-print
-image_text = graph1.pert2image(g, format='png')
-fsalida = open('CohenSadeh' + filename + '.png', 'w')
-fsalida.write(image_text)
-fsalida.close()
+    itime=os.times()
+    for i in range(repeticiones):
+        g=algoritmoConjuntos.algoritmoN(prelaciones1)
+    ftime=os.times()
+    utime = ftime[0] - itime[0]
+    print "Algoritmo Conjuntos"
+    print "utime %.4f"% (utime)
+    print "numero de nodos: ",g.numNodes()
+    print "numero de arcos: ",g.numArcs()
+    print "numero de arcos reales: ",g.numArcsReales()
+    print "numero de arcos ficticios: ",g.numArcsFicticios()
+    print
+    image_text = graph1.pert2image(g, format='png')
+    fsalida = open('grafoConjuntos' + filename + '.png', 'w')
+    fsalida.write(image_text)
+    fsalida.close()
 
-itime=os.times()
-for i in range(REPETICIONES):
-    g=algoritmoConjuntos.algoritmoN(prelaciones1)
-ftime=os.times()
-utime = ftime[0] - itime[0]
-print "Algoritmo Conjuntos"
-print "utime %.4f"% (utime)
-print "numero de nodos: ",g.numNodes()
-print "numero de arcos: ",g.numArcs()
-print "numero de arcos reales: ",g.numArcsReales()
-print "numero de arcos ficticios: ",g.numArcsFicticios()
-print
-image_text = graph1.pert2image(g, format='png')
-fsalida = open('grafoConjuntos' + filename + '.png', 'w')
-fsalida.write(image_text)
-fsalida.close()
-
-itime=os.times()
-for i in range(REPETICIONES):
-    g=algoritmoSalas.salas(prelaciones1)
-ftime=os.times()
-utime = ftime[0] - itime[0]
-print "Algoritmo Salas"
-print "utime %.4f"% (utime)
-print "numero de nodos: ",g.numNodes()
-print "numero de arcos: ",g.numArcs()
-print "numero de arcos reales: ",g.numArcsReales()
-print "numero de arcos ficticios: ",g.numArcsFicticios()
-print
-image_text = graph1.pert2image(g, format='png')
-fsalida = open('algoritmoSalas' + filename + '.png', 'w')
-fsalida.write(image_text)
-fsalida.close()
+    itime=os.times()
+    for i in range(repeticiones):
+        g=algoritmoSalas.salas(prelaciones1)
+    ftime=os.times()
+    utime = ftime[0] - itime[0]
+    print "Algoritmo Salas"
+    print "utime %.4f"% (utime)
+    print "numero de nodos: ",g.numNodes()
+    print "numero de arcos: ",g.numArcs()
+    print "numero de arcos reales: ",g.numArcsReales()
+    print "numero de arcos ficticios: ",g.numArcsFicticios()
+    print 
+    image_text = graph1.pert2image(g, format='png')
+    fsalida = open('algoritmoSalas' + filename + '.png', 'w')
+    fsalida.write(image_text)
+    fsalida.close()
+else:
+    print
+    print "Numero de parametros introducidos erroneo."
+    print
+    print "Ejemplo:"
+    print
+    print "python pruebaAC.py j301_1.sm 1000"
+    print
