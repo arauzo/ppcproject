90e45b107b92f9bd2480e1dd88e3407eec505a0f
perezcaballeroalberto <perezcaballeroalberto@gmail.com>
2011-03-11 09:51:43 +0000

    prueba commit

--- ppcproject.py
+++ ppcproject.py
@@ -19,6 +19,7 @@
  You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 # Python Std. lib.
 import os
 from copy import deepcopy
