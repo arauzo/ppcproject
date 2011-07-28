f9709b94be6c82f5f907885307d30f0b90e54b8b
perezcaballeroalberto <perezcaballeroalberto@gmail.com>
2011-07-28 15:31:03 +0000

    Revision_Memoria_AlbertoPerez

--- Kahn1962.py
+++ Kahn1962.py
@@ -19,43 +19,68 @@ def check_kahn1962(prelations):
     #else 
     #    output message (proposed topologically sorted order: L)
 
-    visitedElements = [] #Empty list that will contain the visited elements
-    noIncomingEdges = graph.beginingActivities(prelations) #Set of all nodes with no incoming edges
-    print prelations
-    print noIncomingEdges
-
-    while len(noIncomingEdges) > 0: #While noIncomingEdges no empty
-        act = noIncomingEdges.pop()
-        visitedElements.append(act) #Remove node n from noIncomingEdges and insert n into visitedElements
-        print "act = " + act
-        print "noIncomingEdges = "
-        print noIncomingEdges
-        print "visitedElements"
-        print visitedElements
-        setPrelations = set(prelations[act])
-        del prelations [act]
-        print prelations
-        b = graph.beginingActivities(prelations, setPrelations)
-        noIncomingEdges.update(b)
-        print 'B:', b #if m has no other incoming edges 
-        print "noIncomingEdges1 = "
-        print noIncomingEdges
+    visited_elements = [] #Empty list that will contain the visited elements
+    no_incoming_edges = graph.beginingActivities(prelations) #Set of all nodes with no incoming edges
+    print "Datos de Entrada\n", prelations, "\n\n"
+    iteracion=1
+    while len(no_incoming_edges) > 0: #While no_incoming_edges no empty
+        act = no_incoming_edges.pop()
+        visited_elements.append(act) #Remove node n from no_incoming_edges and insert n into visited_elements
+        print "Iteracion ", iteracion
+        iteracion+=1
+        print "Prelations: ", prelations
+        print "Activity: ", act
+        print "noIncomingEdges: ", no_incoming_edges
+        print "visitedElements: ", visited_elements, "\n"
+        set_prelations = set(prelations[act]) #Get the new set prelations
+        del prelations[act] #Delete prelations activity
+        no_incoming_edges.update(graph.beginingActivities(prelations, set_prelations)) #Update no_incoming_edges with new begining activities
     if prelations: #if graph has edges
-        print "error" #graph has at least one cycle
-    else:
+        print "ERROR" #graph has at least one cycle
+    else: #graph has no cycles
         print "OK"
         
 # --- Start running as a program
 if __name__ == '__main__':
     prelations = {
         'a' : ['b','c'],
-        'b' : ['e'],
+        'b' : ['d'],
         'c' : ['e'],
-        'd' : ['f'],
-        'e' : ['d','f'],
-        'f' : [],
+        'd' : ['e'],
+        'e' : [],
     }
-
+    
+    simple_cycle = {
+        'a' : ['b','c'],
+        'b' : ['c','d'],
+        'c' : ['e','b'],
+        'd' : ['e'],
+        'e' : [],
+    }
+    
+    cycle = {
+        'a' : ['b'],
+        'b' : ['c','d'],
+        'c' : ['e'],
+        'd' : ['e'],
+        'e' : ['f','h'],
+        'f' : ['g'],
+        'g' : ['h','i'],
+        'h' : ['f','i'],
+        'i' : [],
+    }
+    
+    two_cycles = {
+        'a' : ['b','c'],
+        'b' : ['c','d'],
+        'c' : ['e'],
+        'd' : ['e'],
+        'e' : ['d','c'],
+    }
+    
+#    graph_list = [prelations, simple_cycle, two_cycles]
+    
+#    for graph in graph_list:
     print check_kahn1962(prelations)
 
 
--- conexos.py
+++ conexos.py
@@ -2,37 +2,59 @@
 # -*- coding: utf-8 -*-
 import graph
 
-def check_conexos(prelations):
+def check_conexos(undirected_graph):
     """
+        Check if connected graph
     """
-    visitedElements = [] #Empty list that will contain the visited elements
-    noIncomingEdges = graph.beginingActivities(prelations) #Set of all nodes with no incoming edges
-    print prelations
-    print noIncomingEdges
+    visited_elements = set() #Empty set that will contain the visited elements
+    aux_random = undirected_graph.iterkeys() #Get iterator over the keys of undirected_graph
+    start = set(aux_random.next()) #Create a set with the elements to visit
+    iteration=1
+    while len(start) > 0 : #While there are elements to visit
+        print "\nIteration: ", iteration
+        iteration+=1
+        print "Start: ", start
+        act = start.pop() #Choose the last elements of start set to check
+        print "Activity: ", act
+        visited_elements.add(act) #Add the selected elements to visited 
+        print "Visited Elements: ", visited_elements
+        set_prelations = set(undirected_graph[act]) #Create de set of next activities
+        print "Set prelations", set_prelations
+        start.update(set_prelations.difference(visited_elements)) #Update the nexts elements to visit
+    print "Activity Undirected Graph:", undirected_graph.keys(), "\n"
+    for act in undirected_graph.keys(): #Get keys of undirected_graph
+        if act not in visited_elements: #If the activity has not been visit
+            print "ERROR"
+            break
+        else:
+            print "OK"
 
-    while len(noIncomingEdges) > 0: #While noIncomingEdges no empty
-        act = noIncomingEdges.pop()
-        visitedElements.append(act) #Remove node n from noIncomingEdges and insert n into visitedElements
-        
-        setPrelations = set(prelations[act])
-        del prelations [act]
-        noIncomingEdges.update(setPrelations)
-    if prelations: #if graph has edges
-        print "error" #graph has at least one cycle
-    else:
-        print "OK"
-        
+def conected_to_undirected_prelations(prelations):
+    """
+        Make undirected the conected prelations
+        Return new prelations
+    """
+    print "Datos de Entrada(No conexas): ", prelations
+    conected = {} #New Dictionary
+    for act,sigs in prelations.items(): #Copy the Dictionary
+        conected[act] = list(sigs)
+    
+    for act,sigs in prelations.items(): #Do undirected the conected prelations
+        for sig in sigs:
+            conected[sig].append(act) #Add activity to their next activity
+    print "Prelaciones conexas: ", conected
+    return conected
 # --- Start running as a program
 if __name__ == '__main__':
     prelations = {
         'a' : ['b','c'],
-        'b' : ['e'],
+        'b' : ['c','d'],
         'c' : ['e'],
-        'd' : ['f'],
-        'e' : ['d','f'],
+        'd' : ['g'],
+        'e' : [],
         'f' : [],
+        'g' : [],
     }
-
-    print check_conexos(prelations)
-
-
+    
+    undirected_graph = conected_to_undirected_prelations(prelations)
+    check_conexos(undirected_graph)
