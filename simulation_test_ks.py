15211453a3113c8d63fb7549f5a98aec8628a865
anadrion <anadrion@gmail.com>
2012-02-01 19:59:38 +0000

    tabla de medias completa

--- simulation_test_ks.py
+++ simulation_test_ks.py
@@ -16,17 +16,16 @@ import assignment
 import graph
 import kolmogorov_smirnov
 import math
-import pert
-import zaderenko
-import simulation
 import fileFormats
 import ppcproject
 import numpy
+import pert
+import csv
 
 def load (filename):
 
     formatos = [fileFormats.PPCProjectFileFormat(),fileFormats.PSPProjectFileFormat()]
-    print filename
+    #print filename
     try:
         # Tries to load file with formats that match its extension in format order
         data = None
@@ -60,40 +59,33 @@ def load (filename):
         print 'ERROR: Formato del archivo origen no reconocido', '\n'
         
 
-def save (resultados, filename, inputfile, i):
-
-    f = open(filename,"a")
-    f.write('Nombre del archivo de entrada: ')
-    f.write(str(inputfile))
-    f.write('\n')
-    f.write('Numero de iteraciones realizadas: ')
-    f.write(str(i))
-    f.write('\n')
-    f.write('El valor de comparacion es: ')
-    f.write(str(resultados[0]))
-    f.write('\t' + 'Para un alfa = ' + str(resultados[1]))
-    f.write('\n')
-    f.write('Valor para la normal' + '\t' + 'Valor para la gamma' + '\t'+'\t' + 'Valor para VE')
-    f.write('\n')
-    f.write(str(resultados[2]) + '\t'+'\t'+'\t' + str(resultados[3]) + '\t' + '\t'+'\t' + str(resultados[4]))
-    f.write('\n')
-    distribuciones = ['Normal', 'Gamma', 'Extreme Values']
-    cont = 0
-    for n in range(2,5):
-        if resultados[n] <= resultados [0]:
-            cont += 1
-            f.write('TEST SUPERADO!! La distribucion ' + distribuciones[n-2] + ' ha pasado el test' + '\n')
-    if (cont == 0):
-        f.write('NINGUNA DISTRIBUCION HA PASADO EL TEST' + '\n')
-    f.write('El valor del test usando scipy con la normal es: ' + str(resultados[5]) + '\n')
-    f.write('El valor del test usando scipy con la gamma es: ' + str(resultados[6]) + '\n')
-    f.write('El valor del test usando scipy con la de valores extremos es: ' + str(resultados[7]) + '\n')
-    f.write('\n' + '\n')
+def save (resultados, filename, infile):
     
-    f.close()
-   
+    s = ''
+    if (not checkfile(filename)):
+        if filename[-4:] == '.csv':
+            f = open (filename, 'w')
+        else:
+            f = open(filename + '.csv', 'w')
+        s += 'Archivo'+ ',' + 'm' + ',' + 'm1' + ',' + 'Media Normal' + ',' + 'Sigma Normal' + ',' + 'Pvalue Normal' + ',' + 'Media Gamma' + ',' + 'Sigma Gamma' + ',' + 'Pvalue Gamma' + ',' + 'Media Valores Extremos' + ',' + 'Sigma Valores Extremos' + ',' + 'Pvalue Valores Extremos' + ',' + 'Media Simulacion' + ',' + 'Sigma Simulacion' + '\n'        
+    else:
+        f = open(filename, 'a')
 
-def test (it,activity,alfa_test):
+    s += str(infile) + ',' + str(resultados[0]) + ',' + str(resultados[1]) + ',' + str(resultados[2]) + ',' + str(resultados[3]) + ',' + str(resultados[4]) + ',' + str(resultados[5]) + ',' + str(resultados[6]) + ',' + str(resultados[7]) + ',' + str(resultados[8]) + ',' + str(resultados[9]) + ',' + str(resultados[10]) + ',' + str(resultados[11]) + ',' + str(resultados[12]) + '\n'
+
+    f.write(s)
+    f.close()
+
+
+def checkfile(archivo):
+
+    import os.path
+    if os.path.isfile(archivo):
+        return True
+    else:
+        print False
+
+def test (activity,alfa_test,duracionesTotales):
     
 
     informacionCaminos = []
@@ -114,76 +106,80 @@ def test (it,activity,alfa_test):
 
     #Se calcula el numero de caminos dominantes (segun Dodin y segun nuestro metodo),
     #Se asignan los valores a alfa y beta para poder realizar la funcion gamma
+    #Se asignan la media y la sigma estimadas para la gamma
     m, m1, alfa, beta, mediaestimada, sigma = kolmogorov_smirnov.calculoValoresGamma(informacionCaminos)
-    print 'Media Gamma: ', mediaestimada, '\n'
-    print 'Sigma Gamma: ', sigma, '\n'
 
+    #Se asignan la media y la sigma de la normal
     mediaCritico, dTipicaCritico = kolmogorov_smirnov.calculoMcriticoDcriticoNormal(informacionCaminos)
-    print 'Media Critico: ', mediaCritico, '\n'
-    print 'Sigma Critico: ', dTipicaCritico, '\n'
 
+    #Se asignan la media y la sigma de la simulacion
+    mediaSimulation = numpy.mean(duracionesTotales)
+    sigmaSimulation = numpy.std(duracionesTotales)
 
+    #Si hay mas de un camino candidato a ser critico, se calculan los valores para la funcion de valores extremos
+    #Se asignan la media y la sigma de la funcion de valores extremos
     if (m != 1):
         a, b = kolmogorov_smirnov.calculoValoresExtremos (mediaCritico, dTipicaCritico, m)
         mediaVE, sigmaVE = kolmogorov_smirnov.calculoMcriticoDcriticoEV (a, b)
-        print 'Media VE: ', mediaVE, '\n'
-        print 'Sigma VE: ', sigmaVE, '\n'
-    #Creamos un vector con las duraciones totales para pasarselo al test
-    duracionesTotales = vectorDuraciones(it,activity)
-    print 'Media Simulacion: ', numpy.mean(duracionesTotales), '\n'
-    print 'Sigma Simulacion: ', numpy.std(duracionesTotales), '\n'
+
+    #Se crea un vector vacio para guardar los resultados
     results = []
-    print alfa_test
+    #Se calcula un valor de comparacion para el p-value
     valorComparacion = kolmogorov_smirnov.valorComparacion(alfa_test, len(duracionesTotales))
-    print valorComparacion
-    results.append(valorComparacion)
-    results.append(alfa_test)
+    
+    results.append(m)
+    results.append(m1)
     if (m != 1):
         bondadNormal, bondadGamma, bondadVE, pvalueN, pvalueG, pvalueEV = kolmogorov_smirnov.testKS(duracionesTotales, mediaCritico, dTipicaCritico, alfa, beta, a, b)
-        results.append(bondadNormal)
-        results.append(bondadGamma)
-        results.append(bondadVE)
-        results.append(pvalueN)
-        results.append(pvalueG)
-        results.append(pvalueEV)
+        results.append(round(mediaCritico,6))
+        results.append(round(dTipicaCritico,6))
+        results.append(round(pvalueN[0],6))
+        results.append(round(mediaestimada,6))
+        results.append(round(sigma,6))
+        results.append(round(pvalueG[0],6))
+        results.append(round(mediaVE,6))
+        results.append(round(sigmaVE,6))
+        results.append(round(pvalueEV[0],6))
+        results.append(round(mediaSimulation,6))
+        results.append(round(sigmaSimulation,6))
     else:
         bondadNormal, bondadGamma, bondadVE, pvalueN, pvalueG = kolmogorov_smirnov.testKS(duracionesTotales, mediaCritico, dTipicaCritico, alfa, beta)
-        results.append(bondadNormal)
-        results.append(bondadGamma)
-        results.append(bondadVE)
-        results.append(pvalueN)
-        results.append(pvalueG)
-        results.append('No definido')
+        results.append(round(mediaCritico,6))
+        results.append(round(dTipicaCritico,6))
+        results.append(round(pvalueN[0],6))
+        results.append(round(mediaestimada,6))
+        results.append(round(sigma,6))
+        results.append(round(pvalueG[0],6))
+        results.append('Not defined')
+        results.append('Not defined')
+        results.append('Not defined')
+        results.append(round(mediaSimulation,6))
+        results.append(round(sigmaSimulation,6))
 
     
 
     return results
 
+# Hacer una funcion que devuelva cual ha sido el mejor resultado :def theBest (results):
 
 
-def vectorDuraciones(it,actividad):
-   
-    simulaciones = simulation.simulacion(it, actividad)
-    grafoRenumerado = pert.pertFinal(actividad)
+def load2 (infile2):
 
-    nodosN=[]
-    for n in range(len(grafoRenumerado.successors)):
-        nodosN.append(n+1)
-
-    duraciones = []
-    if simulaciones == None:
-            return
-    else:
-        for s in simulaciones: 
-            matrizZad = zaderenko.mZad(actividad,grafoRenumerado.arcs, nodosN, 0, s)
-            tearly = zaderenko.early(nodosN, matrizZad)  
-            tlast = zaderenko.last(nodosN, tearly, matrizZad)      
-            tam = len(tearly)
-            duracionProyecto = tearly[tam-1]
-            duraciones.append(duracionProyecto)
+    real_simulation_list= []
+    with open(infile2, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            real_simulation_list.append(float(row[0]))
+    #f = open(infile2, 'r')
+    #real_simulation = f.read()
+    #real_simulation_tuple = eval(real_simulation)
+    #real_simulation_list = []
+    #for n in range(len(real_simulation_tuple)):
+    #    real_simulation_list.append (real_simulation_tuple[n])
+    #f.close()
     
-    
-    return duraciones
+    return real_simulation_list
+
 
 def main():
     """
@@ -193,22 +189,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', nargs='?', default=sys.stdin,
                         help='Project file to fill (default: stdin)')
+    parser.add_argument('infile2', nargs='?', default=sys.stdin,
+                        help='Project file to fill (default: stdin)')
     parser.add_argument('outfile', nargs='?', default=sys.stdout,
                         help='Name of file to store new project (default: stdout)')
     parser.add_argument('-a', default=0.5, type=float, 
                         help='Value of constant to generate the alfa of the test (default: 0.05)')
-    parser.add_argument('-i', default=1000,type=int,
-                        help='Number of iterations (default: 1000)')
+
 
     args = parser.parse_args()
 
     act, schedules, recurso, asignacion = load(args.infile)
-    resultados = test(args.i,act, args.a)
-    save(resultados, args.outfile, args.infile, args.i)  
-    print 'We will read from', args.infile
-    print 'We will write to', args.outfile
-    print 'Alfa value for the test is ', args.a
-    print 'Number of iterations are', args.i
+    simulation_results = load2 (args.infile2)
+    #XXX Quitar la a
+    resultados = test(act, args.a, simulation_results)
+    i = len(simulation_results)
+    save(resultados, args.outfile, args.infile)  
+    #print 'We will read from', args.infile
+    #print 'We will write to', args.outfile
+    #print 'Alfa value for the test is ', args.a
+    #print 'Number of iterations are', i
 
     # XXX Use return 1 or any non 0 code to finish with error state
     return 0
