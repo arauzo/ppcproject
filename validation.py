70b70834bf98cdf043585fabe1dd8cca03ce2f4b
perezcaballeroalberto <perezcaballeroalberto@gmail.com>
2013-07-11 08:14:54 +0000

    Añadido validation.py

---
+++ validation.py
@@ -0,0 +1,13 @@
+def check_validation(successors, pert): #Compare in's dict an graph's dict
+    """
+    Compare input dict with dict generated from graph for validate
+    that conection between dicts are correct
+    """
+    successors_generated = pert.pertSuccessors() #Successors generated from graph
+    for activity, successors in successors.items():
+        if not activity in successors_generated: #In's dict and graph's dict have differents activities
+            return False
+        else: #In's dict and graph's dict are different
+            if set(successors) != set(successors_generated[activity]):
+                return False
+    return True
