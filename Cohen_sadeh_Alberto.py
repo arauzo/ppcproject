21ecb11a12b4b9b54b6dc42a2425d7d633627a6b
arauzo <arauzo@3ee892a3-3a47-0410-8b82-87159df38f27>
2014-03-30 02:13:25 +0000

    Cohen-Sadeh remodelado terminado a falta de paralelas

--- Cohen_sadeh_Alberto.py
+++ Cohen_sadeh_Alberto.py
@@ -1,11 +1,13 @@
 """
 Algorithm to draw Graph PERT based on Algorithm Yuval Cohen and Arick Sadeh
 """
-import graph
 import copy
+import collections
+import operator
+
+import graph
 import pert
 import fileFormats
-import collections
 import validation
 
 def cohen_sadeh(prelations):
@@ -17,214 +19,98 @@ def cohen_sadeh(prelations):
     return graph pert.Pert()
     """
     #Step 1. Construct Immediate Predecessors Table
-    work_column = {}
+    work_table = {}
     for act, predecessors in prelations.items():
-        work_column[act] = frozenset(predecessors)
-    print "WC:"
-    for k,v in work_column.items():
-        print k,v
+                          # [Predecesors,     Blocked, Dummy, Successors, Start node, End node]
+        work_table[act] = [set(predecessors), False,   False, None,       None,       None]
 
     #Step 2. Identify Identical Precedence Constraint of Diferent Activities
-    non_blocked = set(work_column.values())        
-    print "NB:"
-    for v in non_blocked:
-        print v
+    visited_pred = set()
+    for act, columns in work_table.items():
+        pred = columns[0] 
+        if pred not in visited_pred:
+            visited_pred.add( frozenset(pred) )
+        else:
+            columns[1] = True
 
     #Step 3. Identify Necessary Dummy Arcs
     dups = set()
-    visited = set()
-    for pred in non_blocked:
-        for act in pred:
-            if act in visited:
-                dups.add(act)
-            visited.add(act)                        
-    print dups        
-    print "........"
-
+    visited_act = set()
+    for columns in work_table.values():
+        pred = columns[0]
+        if not columns[1]: # not blocked
+            for act in pred:
+                if act in visited_act:
+                    dups.add(act)
+                visited_act.add(act)                        
+#    print "DUPS:", dups        
 
     #Step 3.2, 3.3 and 4. Create rows and information for Dummy Arcs
-    for act, predecessors in work_column.items():
-        
+    dummy_counter = collections.Counter()
+    for act, columns in work_table.items():
+        # Avoid blocked
+        if not columns[1]:
+            predecessors = columns[0]
+            if len(predecessors) > 1:
+                for act in predecessors:
+                    if act in dups:
+                        predecessors.remove(act)
+                        dummy_name = act + '-d' + str(dummy_counter[act])
+                        dummy_counter[act] += 1
+                        predecessors.add(dummy_name)
+                        work_table[dummy_name] = [set([act]), False, True, None, None, None]
+                       
+    #Step 5. Creating nodes
+    node = 0
+    pred_to_nodes = {}
+    for act, columns in work_table.items():
+        if not columns[2]: # not dummy
+            pred = columns[0]
+            if frozenset(pred) not in pred_to_nodes:
+                pred_to_nodes[frozenset(pred)] = node
+                columns[4] = node
+                node += 1
+            else:
+                columns[4] = pred_to_nodes[frozenset(pred)]                
 
-    
-    #3.1 Scan work_column and list digit that appear more than once in that column
-    visited = [] #List of visited activities
-    more_than_once = [] #List of activities that appear more than once
-    for predecessors in work_column:
-        for activity in predecessors:
-            if activity != '-': #If activity no blocked
-                if activity in visited: #If activity visited
-                    more_than_once.append(activity) #Add activity duplicate
-                visited.append(activity) #Add activity to list of visited
-    print "More than once: ", more_than_once
-    #Repetitions not found before,requires a dummy arc(paralel dummy)
-    same_predecessor = [] #List of list of activities with same_predecessors
-    visited = [] #List of visited activities
-    for activity, predecessor in prelations.iteritems():
-        if activity not in more_than_once: #No repeated comparison
-            for act, pre in prelations.iteritems():
-                if activity != act and predecessor == pre: #Same predecessors
-                    visited.append(activity)
-                    if act not in visited: #No repeated comparison
-                        #List for activity and act
-                        same_predecessor.append([activity, act])
-    print "Same prdedecessor: ", same_predecessor
-    set_pred = () #Set of predecessors
-    paralel_dummy = [] #List of paralel dummies
-    for activity, predecessor in prelations.iteritems():
-        set_pred = set(predecessor)
-        for set_same_pred in same_predecessor:
-            #Activities with same predecessors and successors
-            intermediate_set = set_pred.intersection(set(set_same_pred))
-            #While more than one activity have some predecessors
-            while len(intermediate_set) > 1: 
-                paralel_dummy.append(intermediate_set.pop())
-    print "Paralel_dummy: ", paralel_dummy
-    #If constraint of more_than_once not is a single activity dummy needed
-    #Rename the dummies with numbers
-    visited = [] #List of visited activities
-    dummy_activities = [] #List of dummy activities
-    activity_dummy = [] #List of activity and dummy
-    for i in range(len(immediate_pred)):
-        #If predecessors are not single
-        if len(immediate_pred[i]) > 1:
-            for j in range(len(immediate_pred[i])):
-                #If activity have more than one constraint 
-                #Or activity is paralel dummy
-                if immediate_pred[i][j] in more_than_once or immediate_pred[i][j] in paralel_dummy:
-                    visited.append(immediate_pred[i][j])
-                    #Count number of dummy ocurrence
-                    number = visited.count(immediate_pred[i][j]) 
-                    dummy = immediate_pred[i][j] #Dummy = activity 
-                    #Rename dummy
-                    immediate_pred[i][j] = immediate_pred[i][j] + '-' + str(number)
-                    activity = immediate_pred[i][j] #Activity = dummy
-                    dummy_activities.append(activity)
-                    activity_dummy.append([activity, dummy])
-    print "Activity_dummy: ", activity_dummy
-    #Create a table with activities and their immediate predecessors
-    activity_predecessor = [] #List of activity and predecessor
-    activities = prelations.keys() #List of activities
-    for i in range(len(activities)):
-        relation = [] #Relation between activity(int) and predecessor
-        relation.append(activities[i])
-        for j in range(len(immediate_pred)):
-            if i == j:
-                relation.append(immediate_pred[j])
-                activity_predecessor.append(relation)
-    print "Activity_predecessor: ", activity_predecessor
+    #Step 6. Associate activities with their end nodes
+    # (a) find one non-dummy successor for each activity
+    for act, columns in work_table.items():
+        for suc, suc_columns in work_table.items():
+            if not suc_columns[2]: 
+                if act in suc_columns[0]:
+                    columns[3] = suc 
+                    break 
 
+    # (b) find end node
+    for act, columns in work_table.items():
+        suc = columns[3]
+        if suc:
+            end_node = work_table[suc][4]
+        else:
+            end_node = node
+            node += 1
+        columns[5] = end_node
 
-    #Step 4. Add Rows and Information for Dummy Arcs
-    for activity in activity_dummy:
-        activity_predecessor.append(activity)
-    print "Step4.Activity_predecessor: ", activity_predecessor
+    #Step 7. Associate dummy arcs with start nodes
+    for act, columns in work_table.items():
+        if columns[2]:
+            pred = iter(columns[0]).next()
+            start_node = work_table[pred][5]            
+            columns[4] = start_node
 
+#    print "WC:"
+#    for k,v in work_table.items():
+#        print k,v
 
-    #Step 5. Number the AOA nodes
-    starting_node = [] #List of starting nodes
-    constraint = [] #List of constraint
-    count = 0 #Count for number activities
-    for activity, predecessor in activity_predecessor:
-        relation = [] #relation between activity and count
-        relation.append(activity)
-        if activity in dummy_activities: #If activity is dummy
-            relation.append('ignore_dummy')
-            starting_node.append(relation)
-        else: #Activity not dummy
-            if len(constraint) == 0: #If no constraints
-                relation.append(count)
-                constraint.append(predecessor)
-                starting_node.append(relation)
-            elif predecessor in constraint: #Constraint repeated
-                for i in range(len(constraint)): #Search constraint
-                    #Relative position is the count
-                    if constraint[i] == predecessor:
-                        relation.append(i)
-                starting_node.append(relation)
-            else: #At lest one constraint, constraints not repeated
-                count += 1
-                relation.append(count)
-                constraint.append(predecessor)
-                starting_node.append(relation)
-
-    #Step 6. Associate Activities with their End Nodes
-    #Finding non-dummy sucessors for the successor column
-    activity_sucessor = [] #List of activity and sucessor
-    visited = [] #List of visited activities
-    for activity, predecessor in activity_predecessor:
-        if activity not in dummy_activities: #If activity is not dummy
-            for pre in predecessor:
-                relation = [] #Relation between predecessor and activity
-                visited.append(pre)
-                relation.append(pre) #Put predecessor like activity
-                relation.append(activity) #Put activity like successor
-                activity_sucessor.append(relation)
-    for activity, predecessor in activity_predecessor:
-        if activity not in visited: #If activity has no predecessor
-            activity_sucessor.append([activity, []])
-
-    #Finding the AOA end node column
-    max_count = 0
-    activity_end = [] #List of activity and end node
-    for activity, predecessor in activity_predecessor:
-        if activity not in dummy_activities: #If activity is not dummy
-            for pre in predecessor:
-                for act, count in starting_node:
-                    if pre == act:
-                        relation = [] #Relation between act and end_node
-                        if act not in activity_end:
-                            relation.append(act)
-                            for act, count in starting_node:
-                                if activity == act:
-                                    if count > max_count: #Calculate max_count
-                                        max_count = count
-                                    relation.append(count)
-                                    activity_end.append(relation)
-    predecessors = [] #List of predecessors
-    followed_dummies = [] #List of followed dummies
-    end_nodes = [] #Lis of end_nodes
-    for act, pre in activity_predecessor: #Get predecessors
-        predecessors.append(pre)
-    for act_suc, sucessor in activity_sucessor:
-        if len(sucessor) < 1: #If number of sucessors < 1
-            if act_suc in predecessors:
-                followed_dummies.append(act_suc)
-            else: #Activities without sucessors
-                end_nodes.append(act_suc)
-    for activity in followed_dummies: #Activity follow only by dummies 
-        relation = [] #Relation between act and end_node
-        max_count += 1
-        activity_end.append([activity, max_count])
-    for act_end in end_nodes: #Activities without sucessors
-        final_node = max_count + 1 #Only final nodes
-        activity_end.append([act_end, final_node])
-        
-    #Step 7. Associate Dummy Arcs with Their Start nodes
-    dummy_end = [] #List of dummy and end
-    for activity, dummy in activity_dummy:
-        for act, end in activity_end: #Add ends for activities
-            if act == dummy:
-                dummy_end.append([activity,end])
-
-    for i in range(len(starting_node)): #Add ends for dummy_activities
-        for dummy1, end in dummy_end:
-            if starting_node[i][0] == dummy1:
-                starting_node[i][1] = end
-
-    #Step 8. Update the table with the new activity names
-    visited = [] #List of visited activities
+    #Step 8. Generate the graph
     graph = pert.Pert() #Graph to draw
-    for activity, start in starting_node:
-        for act, end in activity_end:
-            if activity == act and activity not in visited:
-                visited.append(act)
-                if activity in dummy_activities:
-                    dummy = activity, True
-                else:
-                    dummy = activity, False
-                graph.add_arc([start, end], dummy)
+    for act, columns in work_table.items():
+        _, _, dummy, _, start, end = columns
+        graph.add_arc( (start, end), (act, dummy) )
+
     return graph.renumerar()
+
 
 def test(prelations, graph):
     finished_activities = {}
