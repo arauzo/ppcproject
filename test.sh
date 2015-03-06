b020a21968e69a711a67f50569be83fba98df2c0
perezcaballeroalberto <perezcaballeroalberto@gmail.com>
2015-03-06 12:25:51 +0000

    añadido manual de codigo y retoque de algoritmos

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 for f in ../../examples/*/*.sm; do
-#for f in ../../examples/Elmaghraby/e101.sm; do #ejecutar un archivo para pruebas
     echo "procesando $f"
     python ../../prueba.py -cgs $f >> pruebas_algoritmos_todos.txt
 done
